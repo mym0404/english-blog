222b41f920d295a1197750f68b9498185c5b3eff
MJ Studio <mym0404@gmail.com>
2026-01-06 09:12:32 +0900

    chore: unconscious commit

--- scripts/sync-up.ts
+++ scripts/sync-up.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env zx
 /* eslint-disable max-len */
+import { homedir } from "node:os";
 import { $ } from "zx";
 
 const main = async () => {
+  const obsidianEnglishPath = `${homedir()}/Library/Mobile Documents/iCloud~md~obsidian/Documents/english/`;
   await $`rsync -aH --delete --delete-delay --itemize-changes \\
   --exclude '.DS_Store' \\
   --exclude '.obsidian/' \\
   --include 'docs/***' \\
   --include 'blog/***' \\
+  --include 'assets/***' \\
   --exclude '*' \\
   './content/' \\
-  '/Users/mj/Library/Mobile Documents/iCloud~md~obsidian/Documents/english/'`;
+  ${obsidianEnglishPath}`;
 };
 
 main();
