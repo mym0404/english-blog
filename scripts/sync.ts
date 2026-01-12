ec34c5798ddbcad7c3cd307f2403e30645e5cbab
MJ <mym0404@gmail.com>
2026-01-12 22:59:40 +0900

    chore: unconscious commit 🎲

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env zx
 /* eslint-disable max-len */
 import { homedir } from "node:os";
-import { $ } from "zx";
+import { $, argv } from "zx";
 
 const main = async () => {
   const obsidianEnglishPath = `${homedir()}/Library/Mobile Documents/iCloud~md~obsidian/Documents/english/`;
+  const remotePath = argv.remotePath || obsidianEnglishPath;
   await $`rsync -aH --delete --delete-delay --itemize-changes \\
   --exclude '.DS_Store' \\
   --exclude '.obsidian/' \\
@@ -12,7 +13,7 @@ const main = async () => {
   --include 'blog/***' \\
   --include 'assets/***' \\
   --exclude '*' \\
-  ${obsidianEnglishPath} \\
+  ${remotePath} \\
   './content/'`;
 };
 main();
